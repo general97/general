76ab47e86d3ffc973de4faaac50f5fc8b31b0ebc
general97 <h0951606932@gmail.com>
2017-05-01 13:47:12 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+
+<html><head><link rel="icon" type="image/x-icon" href="http://www.3rbclassic.com/vb/upload/2014/10/181classic.jpg">
+
+<title>Hacked By Syrian Hackers</title>
+	  	  
+</head>
+<body>
+ <BODY onLoad="alert('Hacked By ISPA Team & Syrian Revolution Electronic Army !');" onUnload="alert('Hacked By ISPA Team & Syrian Revolution Electronic Army & & Suliman Hacker !');"> 
+
+<div id="container">
+<div><h1><font color="Red">Hacked By ISPA Team & Syrian Revolution Electronic Army</font> <br> <font color="Green">Hackers Of Syrian Revolution</font></h1></div>
+<center>
+<img src="https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-9/15941042_10154219245405222_3577346403025002714_n.jpg?oh=a00bb2b7d1e1ce5fd319423306dc76f8&oe=5914A13E" style="width:900px;height:300px;">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript">
+</script>
+
+
+</div>
+</center>
+<div><h3><font color="Black">In besiged Aleepo there is nothing but smell of death and sound of guns and explosions which fall on civilians; and Russian warplane which
+bombing the civ.in all kindof weapons. The Syrian government begun executions and bombing everywhere and excute all the medical team in besiged Aleepo 
+In Aleepo, thre is difficultness to describe the life there.In Aleppo the death is preferably on staying in government prisons. All peoples are afraid, they did not ask 
+about anything unless safe exit from fired city. The war did not distinguished between man or wome elder or young mosque or church animals or planet.every things 
+burns without any exceptions.Indeed, our need to definitife solution is increased to the max. there is more than 100,000 civilan trapped in very
+narrow area waiting nothing. Aleppo exterminated with global silence. If we did not move and make something the history will curse us 
+God save Aleppo and Syria</font> <br><h3>
+<div><h1><font color="green"></h1></div>
+<center>
+</center>
+<iframe width="0" height="0" src="https://www.youtube.com/embed/DHm9LV2TcAo?autoplay=1" frameborder="0" allowfullscreen></iframe>
+
+<script language="JavaScript1.2"> 
+/*
+SCRIPT EDITE SUR L'EDITEUR JAVASCRIPT
+http://www.editeurjavascript.com
+*/
+ 
+function ejs_nodroit()
+{
+alert('Hacked By Union Syrian Hackers !');
+return(false);
+}
+ 
+document.oncontextmenu = ejs_nodroit;
+</script> </div>
+<meta http-equiv="Content-Type" content="text/html; charset=utf8">
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Audiowide">
+<style>
+     body {
+		
+        font-family: 'Audiowide', serif;
+		color: green;
+		text-align: center;		
+		}
+
+	a {
+		text-decoration:none;
+		color:blue;
+		}
+	#copy{
+		color: gray;
+		font-size: 12px;
+		}
+	#container{
+		margin-top:3%;
+		}
+		
+
+	  
+</style>
+</font></p>
+<p align="center"><img src="http://cgibin.erols.com/cgi-bin/Count.cgi?df=Dr3awe&amp;ft=6&amp;tr=N&amp;dd=B&amp;md=8"><br>
+</body></html>
